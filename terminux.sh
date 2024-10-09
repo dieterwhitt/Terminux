2d923c039dfbf46b1725320fe4c7147a081baec9
dieter whittingham <dieter.whitt@gmail.com>
2024-10-09 04:55:52 -0400

    added sample animation (splash)

--- terminux.sh
+++ terminux.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
 
-echo "hello from terminux.sh"
+dir="$(pwd)/"
+argc=$#
+
+msg="Hello from terminux.sh!"
+
+# sample animation
+fps=30
+if [ -n "$1" ] && [[ "$1" =~ ^[0-9]+$ ]]; then
+    fps=$(($1))
+fi
+
+tick=$(echo "scale=8; 1/$fps" | bc)
+for (( i=0; i < ${#msg}; i++ )); do
+    printf "%c" "${msg:i:1}"
+    sleep ${tick}
+done
+
+# sleep 5 frames
+sleep $(echo "5 * ${tick}" | bc)
+
+echo
