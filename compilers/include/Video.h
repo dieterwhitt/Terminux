a488ca87bef893a4887e7c656179e270189a65ab
dieter whittingham <dieter.whitt@gmail.com>
2024-10-14 17:29:56 -0400

    video class header (module 2)

---
+++ compilers/include/Video.h
@@ -0,0 +1,19 @@
+// Video.h
+// 10/14/24
+
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Video {
+    private:
+        
+    public:
+        
+};
+
+#endif
